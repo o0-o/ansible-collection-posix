4f6835fc1d0a7ef0fabd716c631c109dfa598a10
oØ.o <o0-o@o0-o.ooo>
2025-09-21 19:49:46 -0400

    style(modules): add  to all modules

--- plugins/modules/mounts.py
+++ plugins/modules/mounts.py
@@ -10,6 +10,7 @@
 # This file is part of the o0_o.posix Ansible Collection.
 
 from __future__ import absolute_import, division, print_function
+from __future__ import annotations
 
 __metaclass__ = type
 
